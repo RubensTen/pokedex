1aced9e3f012936bf7160bb17b73ce4b27877fda
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-09 21:48:19 -0500

    create the home component and remove index file unused

---
+++ src/components/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const Home = () => (
+    <section id="home-wrapper" className="container">
+        <h1>Hello from Home!!</h1>
+    </section>
+);
+// dude when use export default this fail in the use app routing why?
+export { Home };
