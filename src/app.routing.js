436780cf43847f6555d4853d874b143c73d552b9
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-09 21:48:02 -0500

    adding home in routing

--- src/app.routing.js
+++ src/app.routing.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // page components
-import { Login as LoginComponent } from './components/pages';
+import { Home as HomeComponent } from './components/pages/home';
+import { Login as LoginComponent } from './components/pages/login';
 
 // routing function
 const AppRouting = () => (
     <Router>
-        <Route exact path="/" component={LoginComponent} />
+        <div>
+            <Route exact path="/" component={HomeComponent} />
+            <Route exact path="/login" component={LoginComponent} />
+        </div>
     </Router>
 );
 
