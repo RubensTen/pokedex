9cb1f6c02835f558af25a12487e7b7bf1a628ed4
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-11 11:50:04 -0500

    feat(header): update styles with scss and section to header icons

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './index.css';
+// material ui components
+import Icon from '@material-ui/core/Icon';
+
 export class Header extends Component {
     render() {
         return (
@@ -13,6 +16,14 @@ export class Header extends Component {
                     <a href="/about">About</a>
                     <a href="/contact">Contact</a>
                 </nav>
+                <div className="header-icons">
+                    <a href="/login">
+                        <Icon className="icon" color="disabled">account_circle</Icon>
+                    </a>
+                    <a href="/cart">
+                        <Icon className="icon" color="disabled">shopping_cart</Icon>
+                    </a>
+                </div>
             </header>
         );
     }
