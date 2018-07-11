7db71e5cffb005e56d595879c6dbf3027b9b89fa
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-11 11:49:30 -0500

    Implement lazy util to use lazy loading components and implement switch and react fragment in the router of app

--- src/app.routing.js
+++ src/app.routing.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// page components
-import { Home as HomeComponent } from './components/pages/home';
-import { Login as LoginComponent } from './components/pages/login';
+// page lazy components
+import {
+    Home as HomeComponent,
+    Login as LoginComponent,
+    NotFound as NotFoundComponent
+} from './providers/lazyLoader';
+/* import { Home as HomeComponent } from './components/pages/home';
+import { Login as LoginComponent } from './components/pages/login'; */
 
 // routing function
 const AppRouting = () => (
     <Router>
-        <div>
-            <Route exact path="/" component={HomeComponent} />
-            <Route exact path="/login" component={LoginComponent} />
-        </div>
+        <React.Fragment>
+            <Switch>
+                <Route exact path="/" component={HomeComponent} />
+                <Route exact path="/login" component={LoginComponent} />
+                <Route exact path="*" component={NotFoundComponent} />
+            </Switch>
+        </React.Fragment>
     </Router>
 );
 
