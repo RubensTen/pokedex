739ecd7a39d07d2ea20653888b9f492b63d4566c
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-11 21:20:19 -0500

    feature(pages): add new pages
    * new pages
    * add pages with lazy loading

--- src/providers/lazyLoader.js
+++ src/providers/lazyLoader.js
@@ -3,6 +3,18 @@ export const Home = lazy(() =>
     import('../components/pages/home').then(module => module.Home)
 )
 
+export const Shop = lazy(() =>
+    import('../components/pages/shop').then(module => module.default)
+)
+
+export const About = lazy(() =>
+    import('../components/pages/about').then(module => module.default)
+)
+
+export const Contact = lazy(() =>
+    import('../components/pages/contact').then(module => module.default)
+)
+
 export const Login = lazy(() =>
     import('../components/pages/login').then(module => module.Login)
 )
