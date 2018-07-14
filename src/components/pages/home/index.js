ebeb6100774f65e71cb1b5bb7527dfd7803fd9fd
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-14 01:08:48 -0500

    feat(recent-products): add the fake json to render recent products

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,17 +2,88 @@ import React from 'react';
 import './index.css';
 
 import { Banner as BannerComponent } from '../../common/banner';
+import { ProductCard as ProductCardComponent } from '../../common/product/card';
+
+const recentProducts = [
+    {
+        id: 1,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    },
+    {
+        id: 3,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    },
+    {
+        id: 4,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    },
+    {
+        id: 5,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    },
+    {
+        id: 6,
+        title: 'Foo product',
+        description: 'Foo description',
+        price: 1000,
+        images: [
+            'https://images-na.ssl-images-amazon.com/images/I/61rPHfR6c5L._SX679_.jpg'
+        ]
+    }
+];
+const makeRecentProducts = (recentProducts) => {    
+    return recentProducts.map((product) =>
+        <div className="product-item">
+            <ProductCardComponent product={product} key={product.id} />
+        </div>
+    );
+};
+
 const Home = () => (
     <section id="home-wrapper">
         <section id="banner">
             <BannerComponent image={{src: 'assets/img/banner.jpg', alt: 'Landing home'}} />
         </section>
         <hr/>
-        <section>
-            this contain a recen products
+        <section className="recent-products">
+            <h2>Productos recientes</h2>
+            <div className="wrapper-recent-products">
+                {makeRecentProducts(recentProducts)}
+            </div>  
         </section>
-        <section>
-            this contain the brands of products
+        <hr/>
+        <section className="brands">
+            <h2>Nuestras Marcas</h2>
+
         </section>
     </section>
 );
