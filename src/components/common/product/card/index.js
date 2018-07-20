08cc8d1e6493a6e98933c3392aeae79ae0d4fc77
RUBEN PEREZ <rplara@scotiabank.com.mx>
2018-07-20 17:32:33 -0500

    feat(product-card): update the product card component with the carousel slide to show the images of products

--- src/components/common/product/card/index.js
+++ src/components/common/product/card/index.js
@@ -3,27 +3,29 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Carousel from '../../carousel';
 import './index.css';
+
+const makeImageCarousel = (images) => {
+    if(images && images.length > 0) {
+        return <Carousel images={images} />
+    }
+    return <div className="no-image">No images to this product</div>
+};
 
 export class ProductCard extends Component {
     render() {
         return (
             <div className="product-card">
                 <Card className='card'>
-                    <CardMedia
-                        className='media'
-                        image={this.props.product.images[0]}
-                        title={this.props.product.title}
-                    />
+                    {makeImageCarousel(this.props.product.images)}
                     <CardContent>
-                        <Typography gutterBottom variant="headline" component="h2">
+                        <Typography gutterBottom variant="headline" component="h2" className="title">
                             {this.props.product.title}
                         </Typography>
-                        <Typography component="p">
+                        <Typography component="p" className="description">
                             {this.props.product.description}                            
                         </Typography>
                     </CardContent>
